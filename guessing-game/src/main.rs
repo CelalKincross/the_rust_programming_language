7e2ab7a0e39b9d26762365b728ac59acf4950a28
W. Yang-Lee Young <celalkincross@gmail.com>
2023-02-11 10:54:56 +0800

    add loop for multiple guesses and exit when the user wins

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -6,26 +6,31 @@ use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
-    
-    let secret_number = rand::thread_rng().gen_range(1..=100); 
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is {secret_number}");
 
-    println!("Please enter your guess:");
-    let mut guess = String::new();
+    loop {
+        println!("Please enter your guess:");
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
         println!("You guessed: {guess}");
 
         // shadow the guess variable// shadowing allows you to use the guess variable without creating another unique variable and having two guess variables located in memory
-        
+
         let guess: u32 = guess.trim().parse().expect("Please enter a number!");
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("You win!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
         }
+    }
 }
