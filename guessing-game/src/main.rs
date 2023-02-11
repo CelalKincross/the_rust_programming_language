a9650da127fb33bb55c496dc5aba0f66dae350dd
W. Yang-Lee Young <celalkincross@gmail.com>
2023-02-11 10:30:22 +0800

    shadow guess variable and fix type mistmatch

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,12 +1,14 @@
 use std::io;
 // input output library comes from the standard library
+// cargo doc --open  // will build docs of dependencies then just click on the crate you are interested in
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
     
     let secret_number = rand::thread_rng().gen_range(1..=100); 
-    println!("The secret number i s{secret_number}");
+    println!("The secret number is {secret_number}");
 
     println!("Please enter your guess:");
     let mut guess = String::new();
@@ -16,4 +18,14 @@ fn main() {
         .expect("Failed to read line");
 
         println!("You guessed: {guess}");
+
+        // shadow the guess variable// shadowing allows you to use the guess variable without creating another unique variable and having two guess variables located in memory
+        
+        let guess: u32 = guess.trim().parse().expect("Please enter a number!");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
 }
